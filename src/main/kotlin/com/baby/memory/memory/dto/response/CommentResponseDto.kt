80654ea35c5a86dcfc208e5bcc711ae1f4439e2e
mamulee <ghdah7@gmail.com>
2023-10-06 18:35:32 +0900

    commnet isDeleted response

--- src/main/kotlin/com/baby/memory/memory/dto/response/CommentResponseDto.kt
+++ src/main/kotlin/com/baby/memory/memory/dto/response/CommentResponseDto.kt
@@ -6,14 +6,16 @@ data class CommentResponseDto(
     val memberId: Long,
     val memoryId: Long,
     val commentId: Long,
-    val content: String
+    val content: String,
+    val isDeleted: Boolean
 ) {
     companion object {
         fun of(comment: Comment): CommentResponseDto = CommentResponseDto(
             memberId = comment.member.id,
             memoryId = comment.memory.id,
             commentId = comment.id,
-            content = comment.content
+            content = comment.content,
+            isDeleted = comment.isDeleted
         )
     }
 }
